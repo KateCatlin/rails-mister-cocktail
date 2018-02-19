b9b8ed47d41f725a5c279e9f8842c3ab02f0ca2f
Kate Catlin <kcatlin2@gmail.com>
2018-02-19 12:30:06 +0800

    added seeds

---
+++ db/migrate/20180219034809_create_doses.rb
@@ -0,0 +1,10 @@
+class CreateDoses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :doses do |t|
+      t.references :cocktail, foreign_key: true
+      t.references :ingredient, foreign_key: true
+      t.string :description, null: false
+      t.timestamps
+    end
+  end
+end
